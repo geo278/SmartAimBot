6c0ebd3da4963aa381b03bf9d23a97ee7d83ffce
x3w0b <x3w0b@ugrad.cs.ubc.ca>
2019-07-04 19:14:08 -0700

    tested capture, working right mouse detection and screen capture

---
+++ SmartAimBot/Source.cpp
@@ -0,0 +1,69 @@
+// SmartAimBot.cpp : Defines the entry point for the application.
+
+//#include "scan.h"
+#include "iostream"
+#include "Windows.h"
+
+using namespace std;
+
+//bool paused = false;
+
+void capture(POINT a, POINT b) {
+	// copy screen to bitmap
+	HDC     hScreen = GetDC(NULL);
+	HDC     hDC = CreateCompatibleDC(hScreen);
+	HBITMAP hBitmap = CreateCompatibleBitmap(hScreen, abs(b.x - a.x), abs(b.y - a.y));
+	HGDIOBJ old_obj = SelectObject(hDC, hBitmap);
+	BOOL    bRet = BitBlt(hDC, 0, 0, abs(b.x - a.x), abs(b.y - a.y), hScreen, a.x, a.y, SRCCOPY);
+
+	// save bitmap to clipboard
+	OpenClipboard(NULL);
+	EmptyClipboard();
+	SetClipboardData(CF_BITMAP, hBitmap);
+	CloseClipboard();
+
+	// clean up
+	SelectObject(hDC, old_obj);
+	DeleteDC(hDC);
+	ReleaseDC(NULL, hScreen);
+	DeleteObject(hBitmap);
+}
+
+//void SetupBitmapInfo(BITMAPINFO& bmi, int bWidth, int bHeight, int bitsPerPixel);
+//bool CompareColour(RGBQUAD * pPixels, int height, int width, int x, int y);
+//void ScanBMP(Scan * scan);
+
+bool Aim() {
+	POINT a, b;
+	a.x = 720;
+	a.y = 405;
+	b.x = 1200;
+	b.y = 675;
+	while (true) { // while rmb pressed
+		if ((GetKeyState(VK_RBUTTON) & 0x100) != 0) {
+			capture(a, b);
+		}
+
+		// get bitmap
+		// compute offset to first colour match
+		// move mouse
+		// wait duration
+	}
+
+	// !paused && 
+	return true;
+}
+
+//MouseCoordinates CurrentMouseXY(0, 0);
+
+
+int main() {
+	//POINT pos;
+	//GetCursorPos(&pos);
+	//CurrentMouseXY.X = pos.x;
+	//CurrentMouseXY.Y = pos.y;
+
+	Aim();
+	//system("pause");
+	return 0;
+}
