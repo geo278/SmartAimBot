e3b0a4a1b42640e449151923f4e924d1e1199a24
x3w0b <x3w0b@ugrad.cs.ubc.ca>
2019-07-06 13:03:53 -0700

    upper body recognition algorithm is working

--- SmartAimBot/Source.cpp
+++ SmartAimBot/Source.cpp
@@ -49,59 +49,82 @@ RGBQUAD * capture(POINT a, POINT b) {
 	return pixels;
 }
 
+Mat capture(HWND hwnd) { // screen capture code adapted from https://stackoverflow.com/questions/34466993/opencv-desktop-capture
+	HDC hwindowDC, hwindowCompatibleDC;
 
+	int height, width, srcheight, srcwidth;
+	HBITMAP hbwindow;
+	Mat src;
+	BITMAPINFOHEADER  bi;
+
+	hwindowDC = GetDC(hwnd);
+	hwindowCompatibleDC = CreateCompatibleDC(hwindowDC);
+	SetStretchBltMode(hwindowCompatibleDC, COLORONCOLOR);
+
+	//RECT windowsize;    // get the height and width of the screen
+	//GetClientRect(hwnd, &windowsize);
+
+	srcheight = 400;
+	srcwidth = 400;
+	height = 400 / 1;  //change this to whatever size you want to resize to
+	width = 400 / 1;
+
+	src.create(height, width, CV_8UC4);
+
+	// create a bitmap
+	hbwindow = CreateCompatibleBitmap(hwindowDC, width, height);
+	bi.biSize = sizeof(BITMAPINFOHEADER);    //http://msdn.microsoft.com/en-us/library/windows/window/dd183402%28v=vs.85%29.aspx
+	bi.biWidth = width;
+	bi.biHeight = -height;  //this is the line that makes it draw upside down or not
+	bi.biPlanes = 1;
+	bi.biBitCount = 32;
+	bi.biCompression = BI_RGB;
+	bi.biSizeImage = 0;
+	bi.biXPelsPerMeter = 0;
+	bi.biYPelsPerMeter = 0;
+	bi.biClrUsed = 0;
+	bi.biClrImportant = 0;
+
+	// use the previously created device context with the bitmap
+	SelectObject(hwindowCompatibleDC, hbwindow);
+	// copy from the window device context to the bitmap device context
+	StretchBlt(hwindowCompatibleDC, 0, 0, width, height, hwindowDC, 760, 340, srcwidth, srcheight, SRCCOPY); //change SRCCOPY to NOTSRCCOPY for wacky colors !
+	GetDIBits(hwindowCompatibleDC, hbwindow, 0, height, src.data, (BITMAPINFO*)& bi, DIB_RGB_COLORS);  //copy from hwindowCompatibleDC to hbwindow
+
+
+	// avoid memory leak
+	DeleteObject(hbwindow);
+	DeleteDC(hwindowCompatibleDC);
+	ReleaseDC(hwnd, hwindowDC);
+
+	return src;
+}
 
 bool Aim() {
-	POINT a, b;
-	a.x = 760;
-	a.y = 340;
-	b.x = 1160;
-	b.y = 740;
-	RGBQUAD * pixels;
-	int red;
-	int green;
-	int blue;
 	POINT targetPos;
-
-	double radius, angle, diffFactor;
-	diffFactor = 100;
-	int x, y, index;
+	Mat pic;
+	CascadeClassifier upperBody;
+	if (upperBody.load("C:\\Users\\georg\\CPSC\\opencv-4.1.0\\data\\haarcascades\\haarcascade_upperbody.xml") && !upperBody.empty()) {
+		cout << "Successfully loaded haarcascade_upperbody.xml" << endl;
+	}
+	else cout << "Error Loading XML file" << endl;
 
 	while (true) {
-		angle = 2 * 3.1415 / diffFactor;
-		radius = 1;
 		if ((GetKeyState(VK_RBUTTON) & 0x100) != 0) { // while rmb pressed
-			pixels = capture(a, b);
-			for (int i = 0; i < 160000; i++) {
-				x = radius * cos(angle) + 200;
-				y = radius * sin(angle) + 200;
-				angle += 2 * 3.1415 / diffFactor;
-				radius += 1 / diffFactor;
 
-				if (x < 0 || x > 399 || y < 0 || y > 399) {
-					//cout << "OUT OF BOUNDS  " << x << "  " << y << endl;
-					break;
-				}
-				index = y * 400 + x;
+			pic = capture(NULL);
+			vector<Rect> targets;
+			upperBody.detectMultiScale(pic, targets, 1.1, 2, 0 | CV_HAAR_SCALE_IMAGE, Size(20, 20), Size(200, 200));
 
-				red = (int)pixels[index].rgbRed;
-				green = (int)pixels[index].rgbGreen;
-				blue = (int)pixels[index].rgbBlue;
-
-
-				if (red > 230 && green > 230 && blue > 230) {
-					targetPos.x = index % 400;
-					targetPos.y = index / 400;
-					mouse_event(MOUSEEVENTF_MOVE, targetPos.x - 200, targetPos.y - 200, 0, 0); // x and y are deltas, not abs coordinates
-					break;
-				}
-
-				//cout << red << ", " << green << ", " << blue << endl;
+			if (targets.size() > 0) {
+				targetPos.x = targets[0].x + targets[0].width * 0.5;
+				targetPos.y = targets[0].y + targets[0].height * 0.5;
+				mouse_event(MOUSEEVENTF_MOVE, targetPos.x - 200, targetPos.y - 200, 0, 0);
 			}
 		}
-		Sleep(50); // extra buffer time
+		//Sleep(5); // extra buffer time
 	}
-	free(pixels);
+	//free(pixels);
 	return true;
 }
 
