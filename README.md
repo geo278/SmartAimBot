903c32398f7aa8cd8982749ca182420246b4bac6
George Zhang <43013270+geo278@users.noreply.github.com>
2022-12-09 21:29:39 -0800

    test

---
+++ README.md
@@ -0,0 +1,4 @@
+In different repo: `git clone https://github.com/Microsoft/vcpkg.git`
+run bootstrap-vcpkg.bat
+`./vcpkg integrate install`
+`./vcpkg install opencv:x64-windows`
--- SmartAimBot/Source.cpp
+++ SmartAimBot/Source.cpp
@@ -7,6 +7,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <thread>
+#include <vector>
 
 using namespace std;
 using namespace cv;
